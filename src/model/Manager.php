05bd8ccb7f84e05db981df3d2f1776eb0479e9a7
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-03-30 12:23:00 +0200

    gitignore

--- src/model/Manager.php
+++ src/model/Manager.php
@@ -3,12 +3,12 @@ declare(strict_types=1);
 
 class Manager 
 {
-    private function connectDb()
+    protected function connectDb()
     {
         try{
-            $test = "test";
             $db = new PDO("mysql:host=remotemysql.com;dbname=dtBgVDs3Pl;port=3306","dtBgVDs3Pl","gemKW5z2we");
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $db;
 
         } catch (Exception $e){
             die('Error :' .$e->getMessage());
