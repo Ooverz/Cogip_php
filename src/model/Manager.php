33d715e4ed90ec4d3121023f5f95839bb77d9193
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-03-30 10:12:07 +0200

    DataBase

---
+++ src/model/Manager.php
@@ -0,0 +1,16 @@
+<?php 
+declare(strict_types=1);
+
+class Manager 
+{
+    protected function connectDb()
+    {
+        try{
+            $db = new PDO("mysql:host=remotemysql.com;dbname=dtBgVDs3Pl;port=3306","dtBgVDs3Pl","gemKW5z2we");
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (Exception $e){
+            die('Error :' .$e->getMessage());
+        }
+    }
+}
